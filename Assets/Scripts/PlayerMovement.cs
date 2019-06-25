63ac77c613682980b49b2b3c33eb7f5a5a9d184e
QuloZ <akimsiec@gmail.com>
2019-06-25 18:06:09 +0300

    Прыжок

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private GameObject player;
+    private Rigidbody rb;
 
     public int speedWalk;
     private int speed;
+    public float jumpForce;
+    public bool isGround;
 
-    void Start()
+    void Awake()
     {
         player = (GameObject)this.gameObject;
+        rb = gameObject.GetComponent<Rigidbody>();
         speed = speedWalk;
+        
     }
 
     // Update is called once per frame
@@ -46,5 +51,15 @@ public class PlayerMovement : MonoBehaviour
         {
             player.transform.position += player.transform.right * speedWalk * Time.deltaTime;
         }
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            isGround = false;
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) //при необходимости прописать условие, что за коллвйдер вошел
+    {
+        isGround = true;
     }
 }
